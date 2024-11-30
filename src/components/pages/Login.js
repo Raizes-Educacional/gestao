0406340e02f32cf20ead60b53ce5d65014108347
Cléber Severo <clebersevero1998@gmail.com>
2024-11-30 09:28:55 -0300

    chore: add submit error and signup page redirect

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,6 +3,8 @@ import colors from "../../utils/colors";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { LogoH } from "../atoms/LogoH.";
 import { useSupabase } from "../../hooks/useSupabase";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const { useBreakpoint } = Grid;
 
 const loginCardStyle = { backgroundColor: '#FFF', borderRadius: '8px', minHeight: '450px'  }
@@ -10,28 +12,29 @@ const loginFormTitleStyle = { textAlign: 'center', marginBottom: '2rem', fontWei
 const flexCentered = { display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }
 
 export const Login = () => {
-
   const supabase = useSupabase()
-  const { md, lg } = useBreakpoint();
+  const { sm, md, lg } = useBreakpoint();
   
+  const [ loginError, setLoginError ] = useState(false)
 
   const onFinish = async (values) => {
-    console.log('Success:', values)
-
     const { user, error } = await supabase.auth.signInWithPassword({
       email: values.username,
       password: values.password,
     })
 
-    console.log('user: ', user);
-    console.log('error: ', error);
-
+    if(error.__isAuthError) { loginErrorHandle() }
+   
   };
 
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   };
   
+const loginErrorHandle = () => {
+  setLoginError('Usuário ou senha inválidos')
+  setTimeout(() => setLoginError(false), 3500)
+}
   return (
     <Flex 
       justify="center" 
@@ -39,7 +42,7 @@ export const Login = () => {
       style={{ 
         background: `linear-gradient(to bottom right, #e7f0fe, ${colors.primaryLight} 60%, ${colors.secondary})`,
         height: '100vh',
-        padding: '0 30px' 
+        padding: !sm ? '0 15px' : '0 30px' 
       }}
     >
         <Row style={{ ...loginCardStyle, minWidth: lg ? '900px' : '0',}}>
@@ -47,13 +50,12 @@ export const Login = () => {
             xs={24} 
             md={24}
             lg={12} 
-            style={{ ...flexCentered, padding: '30px' }}
+            style={{ ...flexCentered, padding: !sm ? '15px' : '20px' }}
           >
                 <LogoH width={230}/>
                 
                 <h3 style={{ color: '#8898aa', fontWeight: '500', marginTop: !md ? '1rem' : '2rem' }}>
-                Boas-Vindas ao <span style={{ color: colors.primary, textTransform: 'uppercase', marginLeft: 3 }}>Projeto Raízes</span>
-
+                  Boas-Vindas ao <span style={{ color: colors.primary, textTransform: 'uppercase', marginLeft: 3 }}>Projeto Raízes</span>
                 </h3>
           </Col>
 
@@ -61,7 +63,7 @@ export const Login = () => {
             xs={24} 
             md={24}
             lg={12} 
-            style={{ ...flexCentered, backgroundColor: '#e7ecfe', padding: '30px', borderRadius: lg ? '0 8px 8px 0' : '10px',  }}
+            style={{ ...flexCentered, backgroundColor: '#e7ecfe', padding: !sm ? '15px' : '20px', borderRadius: lg ? '0 8px 8px 0' : '10px',  }}
           >
                 <Form
                   name="basic"
@@ -84,6 +86,7 @@ export const Login = () => {
                     <Input
                       placeholder="Usuário"
                       size="large"
+                      style={{ borderColor: loginError ? '#ff4d4f' : '' }}
                       prefix={<UserOutlined style={{color: 'rgba(0,0,0,.25)'}} /> }
                       />
                   </Form.Item>
@@ -100,14 +103,19 @@ export const Login = () => {
                     <Input.Password
                       placeholder="Digite sua senha"
                       size="large"
+                      style={{ borderColor: loginError ? '#ff4d4f' : '' }}
                       prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }}/>} 
                     />
                   </Form.Item>
+                    <h4 style={{ fontWeight: '500', color: '#ff4d4f', opacity: loginError ? 1 : 0, marginBottom: '-20px'}}>{loginError || 'eee'}</h4>
+
 
                   <Form.Item label={null} style={{ marginBottom: !md ? '0' : '' }}>
                     <Button type="primary" htmlType="submit" style={{ width: '100%', marginTop: '1.5rem' }}>
-                      <sapn style={{ fontWeight: '600' }}>Entrar</sapn>
+                      <span style={{ fontWeight: '600' }}>Entrar</span>
                     </Button>
+                    
+                    <h4 style={{  fontWeight: '500', marginTop: '8px', textAlign: 'center'}}><Link to={'/matricula'} style={{ textDecoration: 'underline' }}>Ainda não tenho uma conta</Link></h4>
                   </Form.Item>
                 </Form>
           </Col>
