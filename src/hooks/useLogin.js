f0e7dc4006f8f78dcde56dbc07ca3c2143d5b217
Cléber Severo <clebersevero1998@gmail.com>
2024-12-14 15:51:14 -0300

    feat show toaster on login
    - There will be a toaster for loading and success in the login proccess;
    - The auth error is already being handled by the form itself

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,29 +1,49 @@
-import { useState } from 'react';
-import { useSupabase } from './useSupabase';
+import { useContext, useState } from "react"
+import { useSupabase } from "./useSupabase"
+import { ToasterContext } from "../contexts/ToasterContext"
 
 const useLogin = () => {
   const supabase = useSupabase()
-  
-  const [ loginError, setLoginError ] = useState(false)
+
+  const [loginError, setLoginError] = useState(false)
+
+  const { messageApi } = useContext(ToasterContext)
 
   const onFinish = async (values) => {
+    messageApi.open({
+      type: "loading",
+      content: "Loading...",
+      duration: 0,
+    })
+
     const { error } = await supabase.auth.signInWithPassword({
       email: values.username,
       password: values.password,
     })
-      
-    if(error?.__isAuthError) { loginErrorHandle() }
-  };
+
+    messageApi.destroy()
+
+    if (error?.__isAuthError) {
+      loginErrorHandle()
+      return
+    }
+
+    messageApi.open({
+      type: "success",
+      content: "Usuário autenticado com succeso!",
+      duration: 0,
+    })
+  }
 
   const loginErrorHandle = () => {
-    setLoginError('Usuário ou senha inválidos')
+    setLoginError("Usuário ou senha inválidos")
     setTimeout(() => setLoginError(false), 3500)
   }
-  
+
   return {
     loginError,
-            setLoginError,
-    onFinish
+    setLoginError,
+    onFinish,
   }
 }
 
